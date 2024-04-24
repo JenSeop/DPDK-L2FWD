940f9719d5ca37db8ef102c1ffa318aae1176439
JenSeop <dostwoman@gmail.com>
2024-04-24 15:41:18 +0900

    240424-ntk-02

--- sel1/Hash.c
+++ sel1/Hash.c
@@ -5,11 +5,15 @@
 #define BUCKET_SIZE 10007 // 20011 10007 100003
 #define REV_ENDIAN(n) ((uint16_t)(((n) >> 8) | (n) << 8))
 
-#define PKT 100
+#define PKT 10
 int pkt = PKT;
-int loss = PKT;
 
 struct Bucket* hashTable = NULL; 
+
+struct Traffics {
+    uint64_t rx;
+    uint64_t tx;
+};
 
 struct Tuples {
     uint32_t src_ip;
@@ -21,6 +25,7 @@ struct Tuples {
 
 struct Node {
     struct Tuples tuple;
+    struct Traffics traffic;
     struct Node* next;
 };
 
@@ -41,6 +46,19 @@ uint32_t hashSession(struct Tuples tuple){
     return hash % BUCKET_SIZE;
 }
 
+int compareSession(struct Tuples a, struct Tuples b)
+{
+    return (
+            (
+                (a.src_ip == b.src_ip) && (a.dst_ip == b.dst_ip) ||
+                (a.src_ip == b.dst_ip) && (a.dst_ip == b.src_ip)
+            ) &&
+            (a.src_port == b.src_port) &&
+            (a.dst_port == b.dst_port) &&
+            (a.protocol == b.protocol)
+        );
+}
+
 struct Node* createNode(struct Tuples tuple){
     struct Node* newNode;
 
@@ -53,12 +71,13 @@ struct Node* createNode(struct Tuples tuple){
 }
 
 void createBucket(struct Tuples tuple){
-    
     uint32_t hashIndex = hashSession(tuple);
-    
     struct Node* newNode = createNode(tuple);
-    
-    loss--;
+
+    if(hashTable[hashIndex].head && compareSession(tuple, hashTable[hashIndex].head->tuple) == 0)
+        while(hashTable[hashIndex].head)
+            hashIndex = hashIndex + 1 % BUCKET_SIZE;
+
 
     if (hashTable[hashIndex].count == 0){
         hashTable[hashIndex].count = 1;
@@ -112,7 +131,7 @@ uint32_t searchSession(struct Tuples tuple){
         node = node->next;
     }
     
-    return NULL;
+    return 0;
 }
 
 void display(){
@@ -120,10 +139,9 @@ void display(){
     struct Node* iterator;
     printf("\n========= display start ========= \n");
     for (int i = 0; i<BUCKET_SIZE; i++){
-        int flg = 0;
         iterator = hashTable[i].head;
         if(hashTable[i].count)
-            printf("\nBucket[%d] :\n", i);
+            printf("\nBucket[%d] : \n", i);
         while (iterator != NULL)
         {
             printf("Session[%u]\tsrc_ip %d.%d.%d.%d\tdst_ip %d.%d.%d.%d\tsrc_port %u\tdst_port %u\tproto %u\n",
@@ -139,28 +157,71 @@ void display(){
                 iterator->tuple.protocol
             );
             iterator = iterator->next;
-            flg++;
         }
     }
     printf("\n========= display complete ========= \n");
-    printf("%d\n",loss);
+    /*
+    printf("collision = %d\n",collision);
+    for(int idx = 0; idx <= collision; idx++)
+    {
+        iterator = hashTable[idx].head;
+        while (iterator != NULL)
+        {
+            printf("Session[%u]\tsrc_ip %d.%d.%d.%d\tdst_ip %d.%d.%d.%d\tsrc_port %u\tdst_port %u\tproto %u\n",
+                hashSession(iterator->tuple),
+
+                (iterator->tuple.src_ip>>24) & 0XFF,(iterator->tuple.src_ip>>16) & 0XFF,
+                (iterator->tuple.src_ip>>8) & 0XFF,(iterator->tuple.src_ip>>0) & 0XFF,
+
+                (iterator->tuple.dst_ip>>24) & 0XFF,(iterator->tuple.dst_ip>>16) & 0XFF,
+                (iterator->tuple.dst_ip>>8) & 0XFF,(iterator->tuple.dst_ip>>0) & 0XFF,
+
+                REV_ENDIAN(iterator->tuple.src_port),REV_ENDIAN(iterator->tuple.dst_port),
+                iterator->tuple.protocol
+            );
+            iterator = iterator->next;
+        }
+    }
+    */
 }
 
 int main(){
-    
     hashTable = (struct Bucket *)malloc(BUCKET_SIZE * sizeof(struct Bucket));
-    
+    struct Traffics traffic = {4, 4};
+
     for (int i=0; i < pkt; i++){
         struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
         //struct Tuples tuple = { 3232235777, 2886794753, 53764, 20480, 6};
+        createBucket(tuple);
+    }
+    // eq session 01
+    for (int i=0; i < 5; i++){
+        //struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
+        struct Tuples tuple = { 3232235777, 2886794753, 53764, 20480, 6};
+        createBucket(tuple);
+    }
+    for (int i=0; i < 5; i++){
+        //struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
+        struct Tuples tuple = { 2886794753, 3232235777, 53764, 20480, 6};
+        createBucket(tuple);
+    }
+    // eq session 02
+    for (int i=0; i < 5; i++){
+        //struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
+        struct Tuples tuple = { 3232235786, 2886794753, 53764, 20480, 6};
+        createBucket(tuple);
+    }
+    for (int i=0; i < 5; i++){
+        //struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
+        struct Tuples tuple = { 2886794753, 3232235786, 53764, 20480, 6};
         createBucket(tuple);
     }
 
     display();
 
     for (int i=0; i < pkt; i++){
-        struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
-        //struct Tuples tuple = { 3232235777, 2886794753, 53764, 20480, 6};
+        //struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
+        struct Tuples tuple = { 2886794744, 2886794753, 53764, 20480, 6};
         removeSession(tuple);
     }
 }
