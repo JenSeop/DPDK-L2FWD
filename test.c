a58f4fa0a0826ec46c1daaa06626f5272cee7102
JenSeop <dostwoman@gmail.com>
2024-04-25 11:48:06 +0900

    240425-ntk-13

--- test.c
+++ test.c
@@ -7,30 +7,31 @@ struct Bucket* hashTable;
 int main(){
     hashTable = (struct Bucket *)malloc(NSTEK_BUCKET_SIZE * sizeof(struct Bucket));
     Traffics traffic = {400, 400};
+    int i;
 
-    for (int i=0; i < 10; i++){
+    for (i=0; i < 10; i++){
         Tuples tuple = { 3232235777 + i, 2886794753 - i, 5376, 20480, 6 + (i/10)};
         //struct Tuples tuple = { 3232235777, 2886794753, 53764, 20480, 6};
         nstek_createBucket(tuple, traffic);
     }
     // eq session 01
-    for (int i=0; i < 5; i++){
+    for (i=0; i < 5; i++){
         //struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
         Tuples tuple = { 3232235777, 2886794753, 5376, 20480, 6};
         nstek_createBucket(tuple, traffic);
     }
-    for (int i=0; i < 5; i++){
+    for (i=0; i < 5; i++){
         //struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
         Tuples tuple = { 2886794753, 3232235777, 5376, 20480, 6};
         nstek_createBucket(tuple, traffic);
     }
     // eq session 02
-    for (int i=0; i < 5; i++){
+    for (i=0; i < 5; i++){
         //struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
         Tuples tuple = { 3232235786, 2886794753, 5376, 20480, 6};
         nstek_createBucket(tuple, traffic);
     }
-    for (int i=0; i < 5; i++){
+    for (i=0; i < 5; i++){
         //struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
         struct Tuples tuple = { 2886794753, 3232235786, 5376, 20480, 6};
         nstek_createBucket(tuple, traffic);
@@ -38,7 +39,7 @@ int main(){
 
     nstek_display();
 
-    for (int i=0; i < 10; i++){
+    for (i=0; i < 10; i++){
         //struct Tuples tuple = { 3232235777 + i, 2886794753 - i, 53764, 20480, 6 + (i/10)};
         Tuples tuple = { 2886794744, 2886794753, 53764, 20480, 6};
         nstek_removeSession(tuple);
